dd0be5c8e9d3348b22e4a02e5838786d78e5b261
leciej <mateusz.leciejewski@gmail.com>
2025-12-28 00:26:48 +0100

    klasy_utworzone

---
+++ SolutionOrdersReact.Server/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace SolutionOrdersReact.Server.Models
+{
+    public class Category
+    {
+        public int IdCategory { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public bool IsActive { get; set; }
+
+        // Navigation property
+        public virtual ICollection<Item> Items { get; set; } = new List<Item>();
+    }
+}
---
+++ SolutionOrdersReact.Server/Models/Client.cs
@@ -0,0 +1,14 @@
+﻿namespace SolutionOrdersReact.Server.Models
+{
+    public class Client
+    {
+        public int IdClient { get; set; }
+        public string? Name { get; set; }
+        public string? Adress { get; set; }
+        public string? PhoneNumber { get; set; }
+        public bool IsActive { get; set; }
+
+        // Navigation property
+        public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+    }
+}

5b48536fbf242e772373efbc4e12eeb018e050f6
leciej <mateusz.leciejewski@gmail.com>
2026-01-01 21:04:53 +0100

    dziala_z_api

--- SolutionOrdersReact.Server/Program.cs
+++ SolutionOrdersReact.Server/Program.cs
@@ -11,63 +11,67 @@ namespace SolutionOrdersReact.Server
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            // DbContext
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            // Mapster Configuration
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")
+                )
+            );
+
             ItemMappingConfig.Configure();
             OrderMappingConfig.Configure();
             CategoryMappingConfig.Configure();
             UnitOfMeasurementMappingConfig.Configure();
 
-            // MediatR
-            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly())); 
+            builder.Services.AddMediatR(cfg =>
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly())
+            );
 
-            // CORS - dla development zezwalaj na wszystkie połączenia
             builder.Services.AddCors(options =>
             {
-                options.AddPolicy("AllowAll",
+                options.AddPolicy(
+                    "AllowAll",
                     policy => policy
                         .AllowAnyOrigin()
                         .AllowAnyMethod()
-                        .AllowAnyHeader());
+                        .AllowAnyHeader()
+                );
             });
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Automatyczne zastosowanie migracji przy starcie
             using (var scope = app.Services.CreateScope())
             {
                 try
                 {
-                    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                    var dbContext =
+                        scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
                     dbContext.Database.Migrate();
                 }
                 catch (Exception ex)
                 {
-                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "B��d podczas migracji bazy danych");
+                    var logger =
+                        scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(
+                        ex,
+                        "Błąd podczas migracji bazy danych"
+                    );
                 }
             }
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
             app.UseCors("AllowAll");
             app.UseAuthorization();
             app.MapControllers();
